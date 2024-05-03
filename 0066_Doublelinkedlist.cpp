c30ee4ad0d3c039b779741e0ce6bcfadac0e95da
halimbagusalhakim <halsat132@gmail.com>
2024-05-03 14:31:13 +0700

    membuat conditional if

--- 0066_Doublelinkedlist.cpp
+++ 0066_Doublelinkedlist.cpp
@@ -18,6 +18,20 @@ void addNode() {
 	cin >> newNode->noMhs;
 	cout << "\nEnter the name of the student: ";
 	cin >> newNode->name;
+
+	if (START == NULL || newNode->noMhs <= START->noMhs) {
+		if (START != NULL && newNode->noMhs == START->noMhs) {
+			cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+			return;
+		}
+
+		newNode->next = START;
+		if (START != NULL) {
+			START->prev = newNode;
+		}
+		newNode->prev = NULL;
+		START = newNode;
+	}
 }
 
 
