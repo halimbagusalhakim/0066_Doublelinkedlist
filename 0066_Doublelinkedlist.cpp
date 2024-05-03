8dea30dc96f8873c51aae7c89590d5f070ac6571
halimbagusalhakim <halsat132@gmail.com>
2024-05-03 17:33:15 +0700

    membuat switch case dan int main

--- 0066_Doublelinkedlist.cpp
+++ 0066_Doublelinkedlist.cpp
@@ -174,8 +174,37 @@ int main()
 			cout << "5. Search for a record in the list" << endl;
 			cout << "6. Exit" << endl;
 			cout << "\nEnter your choice (1-6): " << endl;
+			char ch;
+			cin >> ch;
+			switch (ch) {
+			case '1':
+				addNode();
+				break;
+			case '2':
+				deleteNode();
+				break;
+			case '3':
+				traverse();
+				break;
+			case'4':
+				revtraverse();
+				break;
+			case'5':
+				searchData();
+				break;
+			case '6':
+				return 0;
+			default:
+				cout << "\nInvalid option" << endl;
+				break;
+			}
 
 		}
-   }
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
+		}
+		
+	}
+	
 }
 
